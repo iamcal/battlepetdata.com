e4b082df14f269145a9775e24ad1f8f56be2d60e
Cal Henderson <cal@iamcal.com>
2018-04-11 17:10:35 +0000

    set template compilation perms during install

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ ln -s /var/www/html/battlepetdata.com/site.conf /etc/apache2/sites-available/bat
 a2ensite battlepetdata.com
 service apache2 reload
 
+cd /var/www/html/battlepetdata.com
+chgrp www-data templates_c
+chmod g+w templates_c
+
 cd /var/www/html/battlepetdata.com/db
 ./init_db.sh
 
